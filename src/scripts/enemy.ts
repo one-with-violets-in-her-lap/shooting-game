b5cb9e2b8476633dfe19cf5e3aeebbdb37815361
crucials <ensueness@gmail.com>
2024-03-06 12:07:55 +0500

    somehow fixed enemy animation glitching

--- src/scripts/enemy.ts
+++ src/scripts/enemy.ts
@@ -3,6 +3,8 @@ import { player } from '@/scripts/player'
 import { Position } from '@/scripts/types/position'
 
 export class Enemy extends GameObject {
+    private lastMoveAxis = 'y'
+
     constructor(position: Position) {
         super(52, 82, position, {
             domElementClass: 'player-container',
@@ -22,24 +24,27 @@ export class Enemy extends GameObject {
             const playerPosition = player.getPosition()
 
             const newPosition = { x: currentPosition.x, y: currentPosition.y }
+            let newDirection: string | undefined = undefined
 
             if(currentPosition.x < playerPosition.x) {
-                this.domElement.setAttribute('direction', 'right')
+                newDirection = 'right'
                 newPosition.x = newPosition.x + 1
             }
-            else {
-                this.domElement.setAttribute('direction', 'left')
+            else if(currentPosition.x > playerPosition.x) {
+                newDirection = 'left'
                 newPosition.x = newPosition.x - 1
             }
 
             if(currentPosition.y < playerPosition.y) {
-                this.domElement.setAttribute('direction', 'bottom')
+                newDirection = 'bottom'
                 newPosition.y = newPosition.y + 1
             }
-            else {
-                this.domElement.setAttribute('direction', 'top')
+            else if(currentPosition.y > playerPosition.y) {
+                newDirection = 'top'
                 newPosition.y = newPosition.y - 1
             }
+
+            this.domElement.setAttribute('direction', newDirection || 'none')
 
             try {
                 this.setPosition(newPosition)
