46ff2831340f57198685d2d09c9f84df8bc7e2e6
crucials <ensueness@gmail.com>
2024-04-20 12:32:11 +0500

    implemented enemy damage

--- src/scripts/player/bullet.ts
+++ src/scripts/player/bullet.ts
@@ -1,14 +1,25 @@
 import { CollisionError, GameObject } from '@/scripts/game-objects'
 import { Position } from '@/scripts/types/position'
 import { convertDegreesToRadians } from '@/scripts/utils/convert-degrees-to-radians'
+import { Enemy } from '@/scripts/enemy'
 
 export class Bullet extends GameObject {
-    constructor(initialPosition: Position, private readonly angle: number) {
-        super(64, 64, initialPosition, {
-            domElementClass: 'bullet'
-        })
+    private readonly angle
 
-        this.domElement.style.transform = `rotate(${this.angle}deg)`
+    constructor(initialPosition: Position, angle: number) {
+        try {
+            super(64, 64, initialPosition, {
+                domElementClass: 'bullet'
+            })
+
+            this.angle = angle
+            this.domElement.style.transform = `rotate(${this.angle}deg)`
+        }
+        catch(error) {
+            if(error instanceof CollisionError && error.object) {
+                Bullet.damage(error.object)
+            }
+        }
     }
 
     update() {
@@ -17,17 +28,26 @@ export class Bullet extends GameObject {
 
             this.setPosition({
                 x: bulletPosition.x + Math.cos(
-                    convertDegreesToRadians(this.angle)
+                    convertDegreesToRadians(this.angle || 0)
                 ) * 18,
                 y: bulletPosition.y + Math.sin(
-                    convertDegreesToRadians(this.angle)
+                    convertDegreesToRadians(this.angle || 0)
                 ) * 18
             })
 
             super.update()
         }
         catch(error) {
+            if(error instanceof CollisionError && error.object) {
+                Bullet.damage(error.object)
+            }
             this.destroy()
+        }
+    }
+
+    private static damage(object: GameObject) {
+        if(object instanceof Enemy) {
+            object.setHealth(object.getHealth() - 1, true)
         }
     }
 }
