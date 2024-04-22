3db31747d4c2d0230e99c9dc72d693495b67156c
crucials <ensueness@gmail.com>
2024-04-22 17:05:34 +0500

    added bullet's parent class constructor in the catch block

--- src/scripts/player/bullet.ts
+++ src/scripts/player/bullet.ts
@@ -4,48 +4,57 @@ import { convertDegreesToRadians } from '@/scripts/utils/convert-degrees-to-radi
 import { Enemy } from '@/scripts/enemy'
 
 export class Bullet extends GameObject {
-    private readonly angle
+    private readonly angle: number
 
     constructor(initialPosition: Position, angle: number) {
         try {
             super(64, 64, initialPosition, {
-                domElementClasses: ['bullet']
+                domElementClasses: ['bullet'],
             })
 
             this.angle = angle
             this.domElement.style.transform = `rotate(${this.angle}deg)`
         }
         catch(error) {
+            // initializes empty placeholder object and destroys it
+            super(64, 64, { x: 0, y: 0 }, {
+                collision: { disabled: true }
+            })
+            this.angle = angle
+
             if(error instanceof CollisionError && error.object) {
-                Bullet.damage(error.object)
+                this.damage(error.object)
             }
+
+            this.destroy()
         }
     }
 
     update() {
+        const BULLET_SPEED = 18
         try {
             const bulletPosition = this.getPosition()
 
             this.setPosition({
                 x: bulletPosition.x + Math.cos(
                     convertDegreesToRadians(this.angle || 0)
-                ) * 18,
+                ) * BULLET_SPEED,
                 y: bulletPosition.y + Math.sin(
                     convertDegreesToRadians(this.angle || 0)
-                ) * 18
+                ) * BULLET_SPEED
             })
 
             super.update()
         }
         catch(error) {
             if(error instanceof CollisionError && error.object) {
-                Bullet.damage(error.object)
+                this.damage(error.object)
             }
             this.destroy()
         }
     }
 
-    private static damage(object: GameObject) {
+    private damage(object: GameObject) {
         if(object instanceof Enemy) {
             object.setHealth(object.getHealth() - 1, true)
         }
