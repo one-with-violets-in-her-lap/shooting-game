4f6d2a2890269e871dde5816963a574a4509f4f3
crucials <ensueness@gmail.com>
2024-06-01 19:45:28 +0500

    set proper `base` option in vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/shooting-game',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
